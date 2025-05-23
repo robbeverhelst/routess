be2c82e1487f5ee73bb05c6c951e52b4a793d719
Robbe Verhelst <robbe.verhelst@gmail.com>
2025-05-23 08:46:33 +0200

    feat: Introduce Pulumi infrastructure setup for Kubernetes deployment

---
+++ apps/infra/eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  {
+    ignores: ['bin/', 'node_modules/'],
+  },
+]; 
